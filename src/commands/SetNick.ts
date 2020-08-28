e0bf30b6d91d951da591bbce1ef45406d784beda
Cornayy <38261196+Cornayy@users.noreply.github.com>
2020-08-28 23:44:55 +0200

    refactor: set nickname command

--- src/commands/SetNick.ts
+++ src/commands/SetNick.ts
@@ -1,6 +1,6 @@
 import { Message } from 'discord.js';
 import { Command } from '../Command';
-import { IBotClient } from '../types';
+import { IBotClient, IGuild } from '../types';
 import { Logger } from '../utils/Logger';
 import { Guild } from '../models/Guild';
 
@@ -16,38 +16,36 @@ export default class SetNick extends Command {
         });
     }
 
+    private async getGuild(id: string): Promise<IGuild> {
+        const guild = await Guild.findOne({ id }).exec();
+        return guild ? guild : await Guild.create({ id, users: [] });
+    }
+
+    private async setNickname(guild: IGuild, nickname: string, id: string): Promise<void> {
+        const user = guild.users.find(({ id }) => id === id);
+
+        if (user) {
+            user.nickname = nickname;
+        } else {
+            guild.users.push({ id: id, nickname });
+        }
+
+        await guild.save();
+    }
+
     public async run(message: Message, args: any[]): Promise<void> {
         const [nickname] = args;
         const { member } = message;
+        const { id } = member.guild;
 
         if (!nickname) {
             await super.respond(message.channel, 'You have not specified a nickname.');
             return;
         }
 
-        try {
-            const { id } = member.guild;
-            let guild = await Guild.findOne({ id }).exec();
+        const guild = await this.getGuild(id);
 
-            if (!guild) {
-                guild = await Guild.create({ id, users: [] });
-            }
-
-            const user = guild.users.find(({ id }) => id === member.id);
-            if (user) {
-                user.nickname = nickname;
-            } else {
-                guild.users.push({ id: member.id, nickname });
-            }
-
-            await guild.save();
-            await super.respond(message.channel, 'Nickname set!');
-        } catch (err) {
-            await super.respond(
-                message.channel,
-                'Something went wrong while setting your nickname.'
-            );
-            Logger.error(err);
-        }
+        await this.setNickname(guild, nickname, member.id);
+        await super.respond(message.channel, 'Nickname set!');
     }
 }
