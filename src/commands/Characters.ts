f03aa6f675fc0609f737a6d09ba5e52f1d712cf9
Cornayy <38261196+Cornayy@users.noreply.github.com>
2020-09-12 22:07:40 +0200

    changed cooldown

--- src/commands/Characters.ts
+++ src/commands/Characters.ts
@@ -24,7 +24,7 @@ export default class Characters extends Command {
                 'Get all the characters of the entered Ankama nickname. [-d, Dofus], [-dt, Dofus Touch], [-w, Wakfu]',
             category: 'Information',
             usage: client.settings.prefix.concat('characters @user or nickname -d, -dt, -w'),
-            cooldown: 5000,
+            cooldown: 1000,
             requiredPermissions: ['READ_MESSAGES']
         });
     }
